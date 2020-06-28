92ea02771d00d8963f848d0c3d97c1074a29e2de
Mr159 <1594341780@qq.com>
2020-06-28 09:42:07 +0800

    first

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="hello">
+      <div class="btn-area">
+       <a href="javascript:;" class="btn btn--m" @click="addCart">加入购物车</a>
+      </div>
+      <Modal v-bind:mdShow="mdShow" v-on:close="closeModal">
+        <p slot="message">
+          请先的登录,再加入购物车
+        </p>
+        <div slot="btnGroup">
+          <a class="btn btn-m" @click="closeModal">关闭</a>
+          <!-- <a href="javascript:;" class="btn btn-m" @click="closeModal">继续购物</a>
+          <router-link href="javascript:;" class="btn btn-m" to="/cart" >查看购物车</router-link> -->
+        </div>
+      </Modal>
+      <div>
+        <div v-for="(item,index) in list" :key="index">
+            {{item.name}}
+            <img src="'./static/img/'+item.img">
+        </div>
+        <button @click="getData">点击我展开</button>
+      </div>
+      <div>
+        <p>哈哈还可以</p>
+        <h1>哈哈哈</h1>
+        <h1>哈哈哈</h1>
+        <h1>哈哈哈</h1>
+        <h1>哈哈哈</h1>
+        <p>上课的卢萨卡</p>
+      </div>
+      <div>
+        <p>sdmlsa</p>
+      </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios"
+import Modal from "./Modal"
+
+export default {
+  name: 'HelloWorld',
+  components:{Modal},
+  data () {
+    return {
+      mdShow: false,
+      list:[]
+    }
+  },
+  methods:{
+    addCart(){
+            this.mdShow=true
+          } ,
+    closeModal(){
+            this.mdShow=false
+          },
+    getData(){
+      axios.get("../../static/data.json").then(res=>{
+        this.list = res.data.imgData
+        console.log(res)
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,159 @@
+<template>
+  <div>
+
+      <div class="md-modal" v-bind:class="{'md-show':mdShow}">
+          <div class="md-modal-inner">
+            <div>
+              <button class="md-close" @click="closeModal">close</button>
+            </div>
+            <div class="md-content">
+              <div class="confirm-tips">
+                <slot name="message"></slot>
+              </div>
+              <div class="btn-wrap">
+                <slot name="btnGroup"></slot>
+              </div>
+            </div>
+          </div>
+      </div>
+
+      <div class="md-overlay" v-if="mdShow" @click="closeModal">
+        
+      </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name:"Modal",
+    props:["mdShow"],
+    data(){
+        return{
+            
+        }
+    },
+    methods:{
+        closeModal(){
+            this.$emit("close")
+        }
+    }
+}
+</script>
+
+<style scoped>
+.md-modal {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  width: 535px;
+  height: auto;
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  visibility: hidden;
+  z-index: 201;
+}
+
+.md-modal .md-close {
+  position: absolute;
+  top: 7px;
+  right: 7px;
+  width: 34px;
+  height: 34px;
+  -webkit-transform: scale(0.5);
+  -ms-transform: scale(0.5);
+  transform: scale(0.5);
+  text-indent: -8000px;
+}
+.md-modal .md-close:before, .md-modal .md-close:after {
+  position: absolute;
+  top: 16px;
+  left: -4px;
+  content: "";
+  width: 44px;
+  height: 3px;
+  background: #d7d8dd;
+  -webkit-transition: -webkit-transform .5s ease-out;
+  transition: -webkit-transform .5s ease-out;
+  transition: transform .5s ease-out;
+  transition: transform .5s ease-out, -webkit-transform .5s ease-out;
+}
+.md-modal .md-close:before {
+  -webkit-transform: rotate(-45deg);
+  -ms-transform: rotate(-45deg);
+  transform: rotate(-45deg);
+}
+
+.md-modal .md-close:after {
+  -webkit-transform: rotate(45deg);
+  -ms-transform: rotate(45deg);
+  transform: rotate(45deg);
+}
+
+.md-modal .md-close:hover:before, .md-modal .md-close:hover:after {
+  -webkit-transition: -webkit-transform .3s ease-out;
+  transition: -webkit-transform .3s ease-out;
+  transition: transform .3s ease-out;
+  transition: transform .3s ease-out, -webkit-transform .3s ease-out;
+}
+
+.md-modal .md-close:hover:before {
+  -webkit-transform: rotate(-135deg);
+  -ms-transform: rotate(-135deg);
+  transform: rotate(-135deg);
+}
+
+.md-modal .md-close:hover:after {
+  -webkit-transform: rotate(-45deg);
+  -ms-transform: rotate(-45deg);
+  transform: rotate(-45deg);
+}
+.md-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  z-index: 200;
+}
+
+.md-modal .md-modal-inner {
+  padding: 40px 30px;
+  background: #fff;
+  -webkit-transform: translateY(20%);
+  -ms-transform: translateY(20%);
+  transform: translateY(20%);
+  opacity: 0;
+  -webkit-transition: all .3s ease-out;
+  transition: all .3s ease-out;
+}
+
+.md-modal.md-show {
+  visibility: visible;
+}
+
+.md-modal.md-show .md-modal-inner {
+  -webkit-transform: translateY(0);
+  -ms-transform: translateY(0);
+  transform: translateY(0);
+  opacity: 1;
+  -webkit-transition: all .5s ease-out;
+  transition: all .5s ease-out;
+}
+.md-content{
+  margin: 0 auto;
+  width: 200px;
+  height: 100px;
+  border: 1px solid;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+  
+
+
+</style>
